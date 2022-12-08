c086aa0c0b142e4cd9bf5db7dc6799ec664fbadc
JosueP0721 <115521291+JosueP0721@users.noreply.github.com>
2022-12-08 10:57:45 -0500

    Herramientas de optimizar imagenes

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+const {src, dest, watch, parallel} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const plumber = require('gulp-plumber');
+const cache = require('gulp-cache');
+const autoprefixer = require('autoprefixer');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const avif =require('gulp-avif');
+
+function css (done) {
+    src('src/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('build/css'))
+
+    done();
+}
+
+function javascript (done) {
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'))
+
+    done();
+}
+
+function optimages (done) {
+    src('src/img/**/*.{jpg,png}')
+        .pipe(cache(imagemin({optimizationLevel: 3})))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+function convertWebp (done) {
+    const options = {
+        quality: 50
+    }
+    src('src/img/**/*.{jpg,png}')
+        .pipe(webp(options))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+function convertAvif (done) {
+    const options = {
+        quality: 50
+    }
+    src('src/img/**/*.{jpg,png}')
+        .pipe(avif(options))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+function images (done) {
+    src('src/img/**/*.svg')
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+function dev (done) {
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
+    watch('src/img/**/*', convertWebp, convertAvif, images, optimages)
+
+    done();
+}
+
+exports.css = css;
+exports.javascript = javascript;
+exports.images = images;
+exports.optimages = optimages;
+exports.convertWebp = convertWebp;
+exports.convertAvif = convertAvif;
+exports.dev = parallel(css, javascript, convertWebp, convertAvif,  images, optimages, dev);
